eccd993d536e0d10ded9b20497918a421c404051
Mary <55291670+Mhch497@users.noreply.github.com>
2024-11-02 15:52:34 +0500

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -36,16 +36,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs['post_id'])
+
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments
+        post = self.get_post()
+        return post.comments.select_related('author')
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get("post_id")
-        post = get_object_or_404(Post, id=post_id)
-        serializer.save(
-            author=self.request.user, post_id=post.id
-        )
+        post = self.get_post()
+        serializer.save(author=self.request.user, post=post)
 
     def perform_update(self, serializer):
         print(serializer.instance)
