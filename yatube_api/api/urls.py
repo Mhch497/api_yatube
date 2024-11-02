5f942aa6a8552eb64c3b9a42f7cbe7e521aee05e
Mary <55291670+Mhch497@users.noreply.github.com>
2024-11-02 16:02:33 +0500

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,7 +8,7 @@ router = SimpleRouter()
 
 router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d)/comments',
+router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
 
 urlpatterns = [
